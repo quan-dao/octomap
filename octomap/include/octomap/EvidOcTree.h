d140d547accab7768bdeb96db80bd36056302e6a
minh-quan <daominhquan9909@gmail.com>
2020-02-04 16:48:29 +0100

    Impl EvidOcTreeNode

--- octomap/include/octomap/EvidOcTree.h
+++ octomap/include/octomap/EvidOcTree.h
@@ -5,6 +5,9 @@
 #include <octomap/OccupancyOcTreeBase.h>
 #include <iostream>
 
+template<typename T>
+bool isNull(T x){return fabs(x) < 1e-5;}
+
 namespace octomap {
 
 	// forward declaration for "friend"
@@ -15,31 +18,51 @@ namespace octomap {
 	public:
 		friend class EvidOcTree;  
 		
-		struct EvidState {
-			EvidState() : f(0.), o(0.), ign(1.0), conf(0.) {};
-			EvidState(float _f, float _o, float _ign, float _conf) 
-				: f(_f), o(_o), ign(_ign), conf(_conf) {};
+		class EvidMass {
+		public:	
+			EvidMass() : conf(0.), free(0.), occu(0.), igno(1.0) {};
+			EvidMass(float _c, float _f, float _o, float _i) 
+				: conf(_c), free(_f), occu(_o), igno(_i) {};
 
-			float f, o, ign, conf;  // power set of states 
+			void operator= (const EvidMass& rhs){
+				conf = rhs.conf;
+				free = rhs.free; 
+				occu = rhs.occu; 
+				igno = rhs.igno;}
+
+			float conf_() const {return conf;}
+			float free_() const {return free;}
+			float occu_() const {return occu;}
+			float igno_() const {return igno;}
+		
+		protected:
+			float free, occu, igno, conf;  // power set of states 
 		};
 
 		EvidOcTreeNode() : OcTreeNode() {}
+		EvidOcTreeNode(const EvidOcTreeNode& rhs) : OcTreeNode(rhs), mass(rhs.mass) {}	
+		
+		inline EvidMass getMass() const {return mass;}
+		inline void setMass(float _c, float _f, float _o, float _i) {mass = EvidMass(_c, _f, _o, _i);}
+		inline void setMass(const EvidMass& _mass) {mass = _mass;}
 
-		EvidOcTreeNode(const EvidOcTreeNode& rhs) : OcTreeNode(rhs), evid(rhs.evid) {}	
+		inline bool isMassSet() const {return isNull(mass.igno_()-1.0);}
 
+		void copyData(const EvidOcTreeNode& from){
+			OcTreeNode::copyData(from);
+			this->mass = from.getMass();
+		}
+
+		/// update this node's evidential mass according to its children's evidential mass
+		void updateMassChildren();
+
+		EvidOcTreeNode::EvidMass getAverageChildMass() const;
 
 	protected:
-		EvidState evid;
-
-	}
-
-
-
-
+		EvidMass mass;
+		bool occ_before;  // whether occupied in previous pointcloud
+	};
 
 } // end namespace
-
-
-
 
 #endif
--- octomap/src/EvidOcTree.cpp
+++ octomap/src/EvidOcTree.cpp
@@ -1,1 +1,41 @@
 #include <octomap/EvidOcTree.h>
+
+namespace octomap {
+
+	// node implementation ------------------------------------------------------
+	EvidOcTreeNode::EvidMass EvidOcTreeNode::getAverageChildMass() const {
+		float _mc = 0, _mf = 0, _mo = 0, _mi = 0;
+		int num_child = 0;
+
+		if (children != NULL){
+			for (int i=0; i<8; i++){
+				EvidOcTreeNode* child = static_cast<EvidOcTreeNode*>(children[i]);
+				if (child != NULL && child->isMassSet()){
+					_mc += child->mass.conf_();
+					_mf += child->mass.free_();
+					_mo += child->mass.occu_();
+					_mi += child->mass.igno_();
+					num_child ++;
+				}
+			}
+		}
+
+		if (num_child > 0) {
+			_mc /= num_child;
+			_mf /= num_child;
+			_mo /= num_child;
+			_mi /= num_child;
+			return EvidMass(_mc, _mf, _mo, _mi);
+		} else {
+			return EvidMass();
+		}
+	}
+
+	void EvidOcTreeNode::updateMassChildren(){
+		setMass(getAverageChildMass());
+	}
+
+	// tree implementation ------------------------------------------------------
+
+
+}//end namespace
